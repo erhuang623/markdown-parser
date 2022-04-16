6428d54942ca28c0e685b40f0729bed0e0364cc0
Eric Huang <erhuang623@gmail.com>
2022-04-15 19:51:29 -0700

    Tester4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,16 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if(markdown.indexOf("(") == -1 && markdown.indexOf(")") == -1)
+            {
+                toReturn.add("");
+                return toReturn;
+            }   
             int openBracket = markdown.indexOf("[", currentIndex); //0
             int closeBracket = markdown.indexOf("]", openBracket); //6
             int openParen = markdown.indexOf("(", closeBracket); //7
             int closeParen = markdown.indexOf(")", openParen); //29, 23
-            if(markdown.indexOf(")"), closeParen + 1) != -1)
+            if(markdown.indexOf(")", closeParen + 1) != -1)
             {
                 closeParen =  markdown.indexOf(")", closeParen + 1);
             }
---
+++ tester4.md
@@ -0,0 +1,3 @@
+# Title
+
+https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule?authuser=0
